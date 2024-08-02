4f763df236036e438f88288880ae7211ece3c1d7
Anurag-Bharde <anwolfragspitter99@gmail.com>
2024-08-02 14:12:31 +0530

    Post req for notes is done

--- Backend/Route/Land.js
+++ Backend/Route/Land.js
@@ -2,23 +2,29 @@ const express=require("express");
 const { LandingSchema } = require("../../Database/databe");
 const land=express();
 const moment = require('moment');
+land.use(express.json())
 
 land.get("/Intruser",(req,res)=>{
 
 })
 
 land.post("/Intruser",async (req,res)=>{
-    const INotes=req.body.noted;
+    console.log(1)
+    const {INotes}=req.body;
     const timer=new Date();
     const notepad=await LandingSchema.create({
         INote:INotes,
-        timePost:timer,
+        timePost:timer
     })
-    
+    console.log(2)
+    res.status(403).json({MSG:"The Thought has been saved"})
 })
 
 
 
+land.listen(3000,()=>{
+    console.log("The app is running on the port 3000")
+})
 
 // {
 // const a="2024-08-01T13:12:10.165Z"
